f9dc88626ceb9a2181bfbbc7af0f796933c78073
something <skew720@gmail.com>
2025-01-15 20:53:51 +0300

    Update activity.py

--- cogs/activity.py
+++ cogs/activity.py
@@ -57,7 +57,7 @@ class ActivityCog(commands.Cog):
             embed.add_field(name='', value=f'<t:{tstamp - 3600}:t> - <t:{tstamp}:t>', inline=False)
             await channel.send(embed=embed)
 
-    @commands.slash_command(name='topuser', description='Показывает самого активного участника за последний час',)
+    @commands.slash_command(name='topuser', description='Показывает самого активного участника за последний час', contexts=disnake.InteractionContextTypes(guild=True, bot_dm=False, private_channel=False))
     @commands.cooldown(rate=1, per=15, type=commands.BucketType.user)
     async def topuser(self, inter: disnake.ApplicationCommandInteraction):
         if inter.type == disnake.InteractionType.application_command:
@@ -201,7 +201,7 @@ class ActivityCog(commands.Cog):
             )
             print("Сброс 'Reaction counts' в 00:00 по Киеву")
 
-    @commands.slash_command(name='reputation', description='Выводит репутацию участника')
+    @commands.slash_command(name='reputation', description='Выводит репутацию участника', contexts=disnake.InteractionContextTypes(guild=True, bot_dm=False, private_channel=False))
     @commands.cooldown(rate=1, per=15, type=commands.BucketType.user)
     async def reputation(self, inter: disnake.ApplicationCommandInteraction, участник: disnake.Member = None):
         if disnake.InteractionResponse:
