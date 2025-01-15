ee811c844e56761dba73d1e4d7048e9ed491a64a
something <skew720@gmail.com>
2025-01-15 20:47:17 +0300

    Update promocode.py

--- cogs/promocode.py
+++ cogs/promocode.py
@@ -46,7 +46,7 @@ class Promo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name='promocode', description='Взаимодействие с промокодами')
+    @commands.slash_command(name='promocode', description='Взаимодействие с промокодами', contexts=disnake.InteractionContextTypes(guild=True, bot_dm=False, private_channel=False))
     @commands.cooldown(rate=1, per=15, type=commands.BucketType.user)
     async def promo(self, inter):
         pass
