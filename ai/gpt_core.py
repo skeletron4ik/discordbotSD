9506ca5b8d199fb5fa56571f5795d60794122e75
something <skew720@gmail.com>
2025-04-21 20:15:20 +0300

    Add files via upload

---
+++ ai/gpt_core.py
@@ -0,0 +1,33 @@
+from groq import AsyncGroq
+from os import getenv
+from dotenv import load_dotenv
+from sys import exit
+
+client = AsyncGroq(api_key="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")
+model = "llama3-70b-8192"
+
+
+
+async def generate_response(prompt, instructions, error, history=None):
+    try:
+        if history:
+            response = await client.chat.completions.create(
+                model=model,
+                messages=[
+                    {"role": "system", "content": instructions},
+                    {"role": "user", "content": prompt},
+                    *history,
+                ],
+            )
+        else:
+            response = await client.chat.completions.create(
+                model=model,
+                messages=[
+                    {"role": "system", "content": instructions},
+                    {"role": "user", "content": prompt},
+                ],
+            )
+        return response.choices[0].message.content
+    except Exception as e:
+        print(f"Не смог сгенерировать запрос.")
+        return error
